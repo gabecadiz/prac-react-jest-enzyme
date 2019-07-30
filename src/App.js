efa0b20574456c4a05c59d18f85b9f0ea18dc746
Computer <gabecadiz@hotmail.com>
2019-07-29 23:01:28 -0400

    Initial commit of header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import Header from './components/header';
+import './app.scss';
 class App extends Component {
   render() {
-    return <div className='App'>TEST</div>;
+    return (
+      <div className='App'>
+        <Header />
+      </div>
+    );
   }
 }
 
